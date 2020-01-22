749b31d2ed068fa3f2e315da355cd5fde00cef2e
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-22 15:45:35 -0500

    python challenge

--- practce.py
+++ practce.py
@@ -2,7 +2,7 @@ python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti'
 
 #Given the names and grades for each student in a Physics class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
 
-nums_in = input()
-nums_split = nums_in.split()
-nums = map(int, nums_split)
