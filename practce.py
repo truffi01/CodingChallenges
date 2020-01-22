882edd2eb30db431c7c4762a68d58ed44e3e9f48
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-22 15:19:14 -0500

    new problem

--- practce.py
+++ practce.py
@@ -1,2 +1,8 @@
-for i in range(n):
-    print i*i
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+#Given the names and grades for each student in a Physics class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+
+nums_in = input()
+nums_split = nums_in.split()
+nums = map(int, nums_split)
