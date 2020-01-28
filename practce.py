a1aee6b122470879831e18965b7b6fea3526d99f
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-28 13:14:38 -0500

    python challenge

--- practce.py
+++ practce.py
@@ -1,23 +1,6 @@
-arr = []
-for i in range(int(raw_input())):
-    s = raw_input().split()
-    for i in range(1,len(s)):
-        s[i] = int(s[i])
-        
-    
-    if s[0] == "insert":
-        arr.insert(s[1],s[2])
-    elif s[0] == "remove":
-        arr.remove(s[1])
-    elif s[0] == "pop":
-        arr.pop()
-    elif s[0] == "index":
-        print arr.index(s[1])
-    elif s[0] == "count":
-        print arr.count(s[1])
-    elif s[0] == "sort":
-        arr.sort()
-    elif s[0] == "reverse":
-        arr.reverse()
-    elif s[0] == "print":
-        print arr
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(raw_input())
+b = int(raw_input())
+print a/b
+print a%b
+print divmod(a,b)
