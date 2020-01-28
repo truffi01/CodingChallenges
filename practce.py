310d4e8b53f8d4d3f6067743f787bdb47e5139fc
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-28 12:47:06 -0500

    correct code

--- practce.py
+++ practce.py
@@ -4,11 +4,8 @@ for i in range(int(raw_input())):
     for i in range(1,len(s)):
         s[i] = int(s[i])
         
-    if s[0] == "append":
-        arr.append(s[1])
-    elif s[0] == "extend":    
-        arr.extend(s[1:])
-    elif s[0] == "insert":
+    
+    if s[0] == "insert":
         arr.insert(s[1],s[2])
     elif s[0] == "remove":
         arr.remove(s[1])
