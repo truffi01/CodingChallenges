b8b799d831037d64de90b168c48cbf45f6de6af3
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-01 10:20:47 -0500

    code wars challenge maskify

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-
+// return masked string
+function maskify(cc) {
+  
+    if (cc.length <= 4){
+      return cc
+    }
+    
+    for (let i = cc.length-1 ; i >= 0; i--){
+      if (i[cc] > i[cc.length-5]){
+        i[cc] = '#'  
+      }
+    }
+    
+    return cc;
+  }
 
