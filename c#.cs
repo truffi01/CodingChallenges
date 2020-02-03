d574d9ff95ef810c73b310f1311b5fd5cc3c3aa0
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-03 12:05:41 -0500

    c# coding pig latin

--- c#.cs
+++ c#.cs
@@ -1,16 +1,10 @@
 
-  // return masked string
-  public static string Maskify(string cc)
-  {
-  char[] charrArr = cc.ToCharArray();
-  
-  for (int i = 0; i < charrArr.Length - 4; i++)
-  {
-    charrArr[i] = '#'
-  }
-  string result = new string(charrArr);
-  return result;
-  }
-
-  Console.Writeline(Maskify("1234567"))
+using System.Linq;
+public class Kata
+{
+    public static string PigIt(string str)
+    {
+        return string.Join(" ", str.Split(' ').Select(w => w.Substring(1) + w[0] + "ay"));
+    }
+}
 
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function pigIt(str) {
-  var arrayWord = str.split(' ');
-  return arrayWord.map(function(word) {
-    var firstLetter = word.charAt(0);
-    return word.slice(1) + firstLetter + 'ay';
-  }).join(' ');
-}
+v
 
 console.log(pigIt("I love gummies!"));
