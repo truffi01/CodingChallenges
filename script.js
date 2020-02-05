562a64749a54ae69beb35d2e13c2f13090afe784
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-05 11:55:22 -0500

    finished

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const isPP = function(n){
-    const max = Math.ceil(Math.sqrt(n));
-    for (let i = 2; i <= max; i++) {
-      for (let j = 2; j <= max; j++) {
-        if (n === i ** j) {
-          return [i, j];
-        } 
-        if (n < i ** j) {
-            break;
-          }
+function tickets(peopleInLine){
+  
+  let twentyFive = []
+  let fifty = []
+  let oneHundred = []
+  
+  for (let i = 0; i < peopleInLine.length; i++){
+    if (peopleInLine[i] === 25){
+      twentyFive.push(peopleInLine[i])
+    }
+    if (peopleInLine[i] === 50){
+      fifty.push(peopleInLine[i])
+    }
+
+    if (peopleInLine[i] === 50){
+      if (twentyFive.length>1){
+        twentyFive.pop();
+      }
+      else {
+        return "No"
       }
     }
-    return null;
+
+    if (peopleInLine[i] === 100){
+      if (twentyFive.length>1 && fifty.length>1){
+        twentyFive.pop();
+        fifty.pop();
+      }
+    }
+
+    if (peopleInLine[i] === 100){
+      if (twentyFive.length>2){
+        twentyFive.pop();
+        twentyFive.pop();
+        twentyFive.pop();
+        console.log("1")
+      }
+      else {
+        return "No"
+      }
+    }
+
+
   }
+  return 'Yes'
+}
   
-console.log(isPP(81));
+console.log(tickets([25,25,25,100]));
