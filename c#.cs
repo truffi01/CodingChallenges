750eb24f198ab22032463c13574d51c42e27932a
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-05 09:55:35 -0500

    starting new problem

--- c#.cs
+++ c#.cs
@@ -1,10 +1,1 @@
 
-using System.Linq;
-public class Kata
-{
-    public static string PigIt(string str)
-    {
-        return string.Join(" ", str.Split(' ').Select(w => w.Substring(1) + w[0] + "ay"));
-    }
-}
-
