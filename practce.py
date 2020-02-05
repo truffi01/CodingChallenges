0e8acebfed38e92c015cfc87db6ac28afe69c1be
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-05 12:37:37 -0500

    python solution

--- practce.py
+++ practce.py
@@ -1,10 +1,20 @@
-def isPP(n):
-    for i in range(2, n+1):
-        for j in range(2, n+1):
-            if i**j > n:
-                break
-            elif i**j == n:
-                return [i, j]
-    return None
-
-code wars solution
+def tickets(people):
+    change = []
+    try:
+        for cash in people:
+            if cash == 25:
+                change.append(25)
+            if cash == 50:
+                change.remove(25)
+                change.append(50)
+            if cash == 100 and (50 in change):
+                change.remove(50)
+                change.remove(25)
+            elif cash == 100:
+                change.remove(25)
+                change.remove(25)
+                change.remove(25)
+    except: 
+        return "NO"
+    else:
+        return "YES"
