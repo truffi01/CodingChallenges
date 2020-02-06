526376efb9600cc96491853c6681e258e8ddda0c
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-06 11:16:39 -0500

    javascript challenge solved

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //convert underscore and dashes into camel case
 
 function toCamelCase(str){
-  let result = str.split("_|-")
-  return result; 
+  let result; 
+
+  if (str.includes("-")){
+    result = str.split("-")
+  }
+  else {
+     result = str.split("_")
+  }
+
+  resultZero = result[0].toLowerCase();
+  result[0] = resultZero;
+
+  for (let i = 1; i < result.length; i++){
+    let word = result[i].toUpperCase().split("");
+    result[i] = word;
+    for (let j = 1; j < result[i].length; j++){
+      let wordTwo = result[i][j].toLowerCase();
+      result[i][j] = wordTwo
+    }
+    result[i] = result[i].join("");
+  }
+  result = result.join("");
+
+  return result;
   }
 
 
-  toCamelCase("the-stealth-warrior")
-  toCamelCase("The_Stealth_Warrior")
+  console.log(toCamelCase("the-stealth-warrior"));
+  console.log(toCamelCase("The_Stealth_Warrior"));
