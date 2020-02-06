f9084ee9312b07cd460ffca839950e1aa0c74668
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-06 10:27:56 -0500

    new javascript challenge camel case

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-function tickets(peopleInLine) {
-  var bills = [0, 0, 0]
-  for (var i = 0; i < peopleInLine.length; i++) {
-    switch (peopleInLine[i]) {
-      case 25:
-        bills[0]++
-        break
-        
-      case 50:
-        bills[0]--
-        bills[1]++
-        break
-        
-      case 100:
-        bills[1] ? bills[1]-- : bills[0] -= 2
-        bills[0]--
-        break
-    }
-    
-    if (bills[0] < 0) {
-      return 'NO'
-    }
+//convert underscore and dashes into camel case
+
+function toCamelCase(str){
+  let result = str.split("_|-")
+  return result; 
   }
-  
-  return 'YES'
-}
-  
-console.log(tickets([25,25,25,100]));
+
+
+  toCamelCase("the-stealth-warrior")
+  toCamelCase("The_Stealth_Warrior")
