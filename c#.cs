7f8e699a91af8e0b41a57e1002daa693baabde53
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-14 22:09:34 -0500

    c# code

--- c#.cs
+++ c#.cs
@@ -1,13 +1,21 @@
-
- using System.Linq;
-
-namespace Solution
+public static class Kata
 {
-  class Kata
-    {
-      public static int find_it(int[] seq) 
-      {
-        return seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
-      }
-    }
+  public static string ReverseWords(string str)
+  {
+        List<string> sl= new List<string>();// List to hold the reversed words
+        
+        string[] words = str.Split(' ');// convert str to string array
+        
+        foreach(string s in words)  // loop through string array
+        {
+          char[] scharr= s.ToCharArray();//convert string to char array
+          Array.Reverse(scharr);//reverse char array
+          
+          string sreverse= new string (scharr);// create string with char array
+          
+          sl.Add(sreverse);//add string to the list
+        }
+        
+        return string.Join(" ", sl.ToArray());//Return the list as a string
+  }
 }
