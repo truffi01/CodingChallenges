589c733040996e675425fab803068a40e838fdb3
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-14 21:08:31 -0500

    count integers in array

--- c#.cs
+++ c#.cs
@@ -1,1 +1,15 @@
-
+namespace Solution
+{
+  class Kata
+    {
+    public static int find_it(int[] seq) 
+      {
+        foreach (int odd in seq)
+        {
+          if 
+      
+        }
+      }
+       
+    }
+}
--- practce.py
+++ practce.py
@@ -3,7 +3,7 @@ def to_camel_case(text):
     removed = text.replace('-', ' ').replace('_', ' ').split()
     if len(removed) == 0:
         return ''
-    return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
+    return removed[0]+ ''.join([x.capitolize() for x in removed[1:]])
 
 
 
