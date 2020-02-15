be0de2f993cec948eb1a54a914f02aca57e61e52
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-14 21:15:21 -0500

    still c#

--- c#.cs
+++ c#.cs
@@ -4,6 +4,8 @@ namespace Solution
     {
     public static int find_it(int[] seq) 
       {
+        ArrayList integerVowel = new ArrayList();
+
         foreach (int odd in seq)
         {
           if 
