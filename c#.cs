8fc41606d0c4eff3b00574d8034affa95edcd44c
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-15 09:04:57 -0500

    c# coding challenge

--- c#.cs
+++ c#.cs
@@ -1,21 +1,9 @@
-public static class Kata
-{
-  public static string ReverseWords(string str)
+  using System;
+  public class Sum
   {
-        List<string> sl= new List<string>();// List to hold the reversed words
-        
-        string[] words = str.Split(' ');// convert str to string array
-        
-        foreach(string s in words)  // loop through string array
-        {
-          char[] scharr= s.ToCharArray();//convert string to char array
-          Array.Reverse(scharr);//reverse char array
-          
-          string sreverse= new string (scharr);// create string with char array
-          
-          sl.Add(sreverse);//add string to the list
-        }
-        
-        return string.Join(" ", sl.ToArray());//Return the list as a string
+     public int GetSum(int a, int b)
+     {
+       
+       return 0;
+     }
   }
-}
