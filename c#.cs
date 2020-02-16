8372114aa500603dce373aba5a4d5e45b6ea2faa
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-16 09:45:43 -0500

    single line c#

--- c#.cs
+++ c#.cs
@@ -6,28 +6,7 @@ public class Kata
 {
   public static int Find(int[] integers)
   {
-    int even = 0;
-    int odd = 0; 
-    
-    foreach(int var in integers)
-    {
-      if (var % 2 == 0)
-      {
-        even++;
-      }
-      if (var % 2 == 0)
-      {
-        odd++; 
-      }
-      
-      if (even>1 && odd == 1)
-      {
-        return var;
-      }
-      if (odd>1 && even == 1)
-      {
-        return var;
-      }
-      
-    }
+       return integers.Count(a=>a%2==1)>1?integers.Single(a=>a%2==0):integers.Single(a=>a%2==1);
+
   }
+}
