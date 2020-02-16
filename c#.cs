350381d0c293a442471cdc86454e3cc0d6604d1e
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-16 09:28:55 -0500

    basic

--- c#.cs
+++ c#.cs
@@ -6,6 +6,28 @@ public class Kata
 {
   public static int Find(int[] integers)
   {
-    return -1;
+    int even = 0;
+    int odd = 0; 
+    
+    foreach(int var in integers)
+    {
+      if (var % 2 == 0)
+      {
+        even++;
+      }
+      if (var % 2 == 0)
+      {
+        odd++; 
+      }
+      
+      if (even>1 && odd == 1)
+      {
+        return var;
+      }
+      if (odd>1 && even == 1)
+      {
+        return var;
+      }
+      
+    }
   }
-}
