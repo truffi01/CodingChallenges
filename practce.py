9f8771440eec29c6cc30d96946d876037f0d9bb6
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-16 10:29:30 -0500

    alternate solution python

--- practce.py
+++ practce.py
@@ -1,16 +1,7 @@
 def find_outlier(integers):
-    determinant = [x for x in integers[:3] if x % 2 == 0]
-    print(determinant)
-    if len(determinant) > 1:
-        # most are even, find the first odd
-        mod = 1
-    else:
-        # most are odd, find the first even
-        mod = 0
-    for i in integers:
-        if i % 2 == mod:
-            return i
-
+    odd = filter(lambda x: x % 2 == 1, integers)
+    even = filter(lambda x: x % 2 == 0, integers)
+    return odd[0] if len(odd) == 1 else even[0]
 
 
 
