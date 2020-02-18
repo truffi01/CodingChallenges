e1a4d092f48d608dde2876f10d0182c1c5869a80
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-18 12:07:12 -0500

    various answers

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //range extraction
 
 function solution(list){
+  //sort iwll sort the numbers from least to greatest
+  let sortedList = list.sort((a,b) => a-b);
+  console.log(sortedList);
+  let result = []
+  //code for the range of three numbers
+  for (let i = 0; i <sortedList.length; i++){
+    if (Math.abs(sortedList[i] - sortedList[i+1] - sortedList[i+2]) === 1) {
+      result.push(sortedList[i], sortedList[i+1],sortedList[i+2])
+      i+=2;
+    }
+
+    if (sortedList[i+2] - sortedList[i+1] + sortedList[i] === sortedList[i+1]){
+      result.push(sortedList[i], sortedList[i+1],sortedList[i+2])
+      
+    }
+
+
+  }
+console.log(result);
 
 }
 
 
-console.log();
+console.log(solution([-4,-3,-2,0,1,4,6,7,8,9,10,14,15]));
