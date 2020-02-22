e8e4a853bd3078371bf40979676e4fdd38b83c71
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-22 10:02:04 -0500

    solution

--- c#.cs
+++ c#.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Linq;
-
 public class Dubstep
     {
         public static string SongDecoder(string input)
-        { 
-           
+        {
+          // Replace each "WUB" with space. Split into words. Toss empty words. Recombine with space in between.
+          return input.Replace("WUB", " ").Split(' ').Where(w => w.Length > 0).Aggregate((a, b) => a + " " + b);
         }
     }
