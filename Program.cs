476a2ef1875dc7bb00e637f4b0840b2a6619654b
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-24 13:34:23 -0500

    solution for fighter

--- Program.cs
+++ Program.cs
@@ -1,28 +1,42 @@
 ﻿using System;
-using System.Collections.Generic; 
+using System.Linq;
 
 namespace practice
 {
-    class Program
+    public class practice 
     {
         static void Main(string[] args)
         {
-            
+            Fighter fighter1 = new Fighter ("Tom",5,5);
+            Fighter fighter2 = new Fighter ("Jeff",5,5);
+            Console.WriteLine(declareWinner(fighter1, fighter2, "Jeff"));
+            Console.Read();
+              
+        }
+        public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker) 
+        {  
+           
+            var fighters = new [] { fighter1, fighter2 };
     
-        static int NbDig(int n, int d) 
-        {
-            int counter = 0;
-            
-            for (int i = 0; i <= n; i++)
-            foreach(char c in (i * i).ToString())
-                if (c == char.Parse(d.ToString()))
-                counter++;
-                
-            return counter;
+            var Attacker = fighters.Single(f => f.Name == firstAttacker);
+            var Defender = fighters.Single(f => f.Name != firstAttacker);
+            Defender.Health -= Attacker.DamagePerAttack;
+            if(Defender.Health <= 0)
+            {
+            return Attacker.Name;
+            }
+            return declareWinner(fighter1, fighter2, Defender.Name);
+        }
+        public class Fighter {
+            public string Name;
+            public int Health, DamagePerAttack;
+            public Fighter(string name, int health, int damagePerAttack) {
+                this.Name = name;
+                this.Health = health;
+                this.DamagePerAttack = damagePerAttack;
+            }
         }
 
-            Console.WriteLine(NbDig(4,1));
-            
-        }
+
     }
 }
