b3ff658762416f44d90cce83bd773d83756563bf
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-25 10:49:25 -0500

    solution

--- Program.cs
+++ Program.cs
@@ -13,30 +13,13 @@ namespace practice
         }
 
         public static long QueueTime(int[] customers, int n)
+        public static long QueueTime(int[] customers, int n)
         {
-             if (n >= customers.Length)
-            {
-               
-                return customers.Max();
-            }
-
-            if (n < customers.Length && n >1)
-            {
-                int Timey = 0; 
-                var Result = Array.Sort(customers);
-                for (int x = 0; x < Result.Length; x + 2)
-                {
-                    Timey += Result[x];
-                    Timey += Result[x+1];
-                }
-                return Timey; 
-            }
-
-            if (n == 1)
-            {
-                return customers.Sum();
-            }
-            return 0;
+        int[] till = new int[n];         
+        for(int i=0;i<customers.Length;i++){       
+            till[Array.IndexOf(till, till.Min())]+=customers[i];
+        }
+        return till.Max();
         }
     }
 }
