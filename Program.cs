049e21a5a32e0985730e6e8fc9273467cac8bbd8
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-25 08:08:44 -0500

    new c# problem

--- Program.cs
+++ Program.cs
@@ -7,35 +7,17 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Fighter fighter1 = new Fighter ("Tom",5,5);
-            Fighter fighter2 = new Fighter ("Jeff",5,5);
-            Console.WriteLine(declareWinner(fighter1, fighter2, "Jeff"));
-            Console.Read();
-              
+            Console.WriteLine(Solution());
+            Console.Read(); 
         }
-        public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker) 
-        {  
-           
-            var fighters = new [] { fighter1, fighter2 };
-    
-            var Attacker = fighters.Single(f => f.Name == firstAttacker);
-            var Defender = fighters.Single(f => f.Name != firstAttacker);
-            Defender.Health -= Attacker.DamagePerAttack;
-            if(Defender.Health <= 0)
+
+        
+        public static int Solution(string roman)
             {
-            return Attacker.Name;
+                throw new NotImplementedException();
             }
-            return declareWinner(fighter1, fighter2, Defender.Name);
-        }
-        public class Fighter {
-            public string Name;
-            public int Health, DamagePerAttack;
-            public Fighter(string name, int health, int damagePerAttack) {
-                this.Name = name;
-                this.Health = health;
-                this.DamagePerAttack = damagePerAttack;
-            }
-        }
+        
+        
 
 
     }
