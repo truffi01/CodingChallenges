f9a35ab195c56dda14a9a6dafe2f37c72341e316
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-25 09:47:13 -0500

    c# shopping problem

--- Program.cs
+++ Program.cs
@@ -7,60 +7,21 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Solution("XIX"));
+            Console.WriteLine(QueueTime([2,3,4], 3));
             Console.Read(); 
         }
 
+        public static long QueueTime(int[] customers, int n)
+        {
+            if (n >= customers.Length)
+            {
+                
+            }
+            
+            
+            return int;
+        }
         
-        public static int Solution(string roman)
-            {
-                int result = 0;
-            char[] charArr; 
-            charArr = roman.ToCharArray(); 
-            foreach (char x in charArr)
-            {
-            if(x == 'I')
-            {
-                result += 1;
-            }
-            
-            else if(x == 'V')
-            {
-                result += 5;
-            }
-            
-            else if(x == 'X')
-            {
-                result += 10;
-            }
-            
-            else if(x == 'L')
-            {
-                result += 50;
-            }
-            
-            else if(x == 'C')
-            {
-                result += 100;
-            }
-            
-            else if(x == 'D')
-            {
-                result += 500;
-            }
-            
-            else if(x == 'M')
-            {
-                result += 1000;
-            }
-            else 
-            {
-                result += 0; 
-            }
-            
-            }
-            return result; 
-            }
 
     }
 }
