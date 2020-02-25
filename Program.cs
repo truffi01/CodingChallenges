384564ec346d1918d7cb9ee38094e2110c8230e6
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-25 10:39:41 -0500

    solution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Systems.Collections.Generic; 
 
 namespace practice
 {
@@ -7,21 +8,35 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(QueueTime([2,3,4], 3));
+            Console.WriteLine(QueueTime([2,3,4], 2));
             Console.Read(); 
         }
 
         public static long QueueTime(int[] customers, int n)
         {
-            if (n >= customers.Length)
+             if (n >= customers.Length)
             {
-                
+               
+                return customers.Max();
             }
-            
-            
-            return int;
-        }
-        
 
+            if (n < customers.Length && n >1)
+            {
+                int Timey = 0; 
+                var Result = Array.Sort(customers);
+                for (int x = 0; x < Result.Length; x + 2)
+                {
+                    Timey += Result[x];
+                    Timey += Result[x+1];
+                }
+                return Timey; 
+            }
+
+            if (n == 1)
+            {
+                return customers.Sum();
+            }
+            return 0;
+        }
     }
 }
